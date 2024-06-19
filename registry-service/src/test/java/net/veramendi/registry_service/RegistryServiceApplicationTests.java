c59381188a95be6423fb9bdea410740654592cd3
Gerson Marcelo Veramendi Santalla <gveramendi@gveramendi.max>
2024-06-18 20:42:04 -0400

    create registry service.

---
+++ registry-service/src/test/java/net/veramendi/registry_service/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.veramendi.registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
